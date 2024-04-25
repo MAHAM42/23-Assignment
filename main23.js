59cdb44977e8af3a9922763f253b3c2a191d11bc
Maham Shahid <161121868+MAHAM42@users.noreply.github.com>
2024-04-25 08:13:50 -0700

    23-Assignment
    TypeScript 45-Assignment of Q23 with Node.js

---
+++ main23.js
@@ -0,0 +1,52 @@
+"use strict";
+// ASSIGNMENT NO 23:
+Object.defineProperty(exports, "__esModule", { value: true });
+/*
+Conditional Tests: Write a series of conditional tests.
+Print a statement describing each test and your prediction for the results of each test.
+ Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+*/
+console.log("ASSIGNMENT NO 23:");
+//Making a Variebles:
+let five = 5;
+let ten = 10;
+// Test 1:
+console.log("Test 1: Five is equal to 5?");
+console.log(five == 5);
+// Test 2:
+console.log("\nTest 2: Ten is equal to 10?");
+console.log(ten == 10);
+// Test 3:
+console.log("\nTest 3: Five is not Equal to 10");
+console.log(five != 10);
+// Test 4:
+console.log("\nTest 4: Ten is greater than 5");
+console.log(ten > 5);
+// Test 5:
+console.log("\nTest 5: Five is smaller than 10");
+console.log(five < 10);
+// Test 6:
+console.log("\nTest 6: Ten is samller than 5");
+console.log(ten < 5);
+// Test 7:
+console.log("\nTest 7: Five is Equal to 10");
+console.log(five == 10);
+// Test 8:
+console.log("\nTest 8: Ten is not equal to 10");
+console.log(ten != 10);
+// Test 9:
+console.log("\nTest 9: Five is greater than 10");
+console.log(five > 10);
+// Test 10:
+console.log("\nTest 10: 100 is smaller than 50");
+console.log(100 < 50);
